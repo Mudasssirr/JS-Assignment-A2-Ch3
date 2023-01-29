6c5917e3fc5ad4440d99322ae46ceba29aa95393
M. Mudassir Awan <121383917+Mudasssirr@users.noreply.github.com>
2023-01-29 10:18:56 +0500

    added JavaScript A2 Ch3
    added JavaScript Assignment 2 from Chapter 3

---
+++ JavaScript A2 Ch3/app.js
@@ -0,0 +1,22 @@
+//Chapter 3
+
+//Question 1
+//var age = "19";
+//alert (age);
+
+//Question 2
+//var track = "You have visited this site 1 time"
+//alert (track);
+
+//Question 3
+//var birthYear = "22-Aug-2003";
+//document.write (birthYear);
+
+//Question 4
+//var vistorsName = " John Dave";
+//var productTitle = " ordered 5 T-shirt(s)";
+//var Quantity = " on XYZ Clothing store";
+
+//document.write (vistorsName);
+//document.write (productTitle);
+//document.write (Quantity);
